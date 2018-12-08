40c3214cf6fb05121e0776099870f31edb6fdc73
dagan <dagan@pumpkin.com>
2018-12-08 18:34:20 +0000

    Latest

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-const express    = require('express')
-const serveIndex = require('serve-index')
+// Security stuff
 const fs = require('fs');
+const consts = require('constants');
 const https = require("https");
 const private_key = fs.readFileSync("/etc/letsencrypt/live/butterypumpkin.com/privkey.pem");
 const full_chain = fs.readFileSync("/etc/letsencrypt/live/butterypumpkin.com/fullchain.pem");
-const cred = {cert: full_chain, key: private_key};
+const security_options = consts.SSL_OP_NO_TLSv1 | consts.SSL_OP_NO_TLSv1_1;
+const cred =
+	{
+	cert: full_chain,
+	key: private_key,
+	secureOptions: security_options,
+	};
 
+// Choose port
+const args = process.argv.slice(2);
+const port = args[0]?args[0]:8443;
+
+// Application
+const express    = require('express')
+const serveIndex = require('serve-index')
 const app = express()
 
 // Serve URLs like /ftp/thing as public/ftp/thing
@@ -14,4 +27,5 @@ const app = express()
 app.use('/', express.static('public/'), serveIndex('public/', {'icons': true, 'view':'details', 'stylesheet':'style.css'}))
 
 // Listen
-https.createServer(cred, app).listen(8443);
+console.log(`Launching on Port ${port}`)
+https.createServer(cred, app).listen(port);
