c28830b55f811fd5e9cdffed45ad2ba916ecd316
dagan <dagan@pumpkin.com>
2019-10-30 13:47:21 +0000

    torrents.js: don't update elements that don't change. Use constants instead of magic numbers

--- static/torrents/torrents.js
+++ static/torrents/torrents.js
@@ -1,13 +1,18 @@
 "use strict";
+const TORRENT_PROGRESS_POLL_PERIOD = 100; // in milliseconds
+const HTTP_STATUS_OK = 200;
+const KIBIBYTE = 1024;
+const MEBIBYTE = KIBIBYTE**2;
+
 function get_request(url, callback)
 {
-	const xmlhttp=new XMLHttpRequest();
+	const xmlhttp = new XMLHttpRequest();
 	xmlhttp.open("GET", url, true);
 	xmlhttp.onreadystatechange = function()
 	{
 		if (xmlhttp.readyState == 4)
 		{
-			 if(xmlhttp.status == 200)
+			 if(xmlhttp.status == HTTP_STATUS_OK)
 				callback(null, xmlhttp.responseText);
 			else
 				callback(Error("Status "+xmlhttp.status));
@@ -18,14 +23,14 @@ function get_request(url, callback)
 
 function post_request(url, parameters, callback)
 {
-	const xmlhttp=new XMLHttpRequest();
+	const xmlhttp = new XMLHttpRequest();
 	xmlhttp.open("POST", url, true);
 	xmlhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 	xmlhttp.onreadystatechange = function()
 	{
 		if (xmlhttp.readyState == 4)
 		{
-			 if(xmlhttp.status == 200)
+			 if(xmlhttp.status == HTTP_STATUS_OK)
 				callback(null, xmlhttp.responseText);
 			else
 				callback(Error("Status "+xmlhttp.status));
@@ -41,30 +46,41 @@ function add_torrent(url, callback)
 
 function update_torrent(torrent)
 {
-	let row = document.getElementById(torrent.infoHash);
 	if(torrent == null || torrent.infoHash == null)
 		return;
+
+	let row = document.getElementById(torrent.infoHash);
+
 	// If doesn't exist, create by cloning tempalate
 	if(row == null)
 	{
+		const dirpath = "/"+torrent.path+"/"+torrent.name;
+
+		// Create torrent entry
 		row = document.getElementById("row_template").cloneNode(true);
 		row.setAttribute("id", torrent.infoHash);
+		
+		// Set torrent attributes that *don't change* 
+		row.querySelector("#name").innerHTML = torrent.name;
+		row.querySelector("#link").setAttribute("href", dirpath);
+
+		// Append to main div
 		row.removeAttribute("hidden");
 		document.getElementById("rows").appendChild(row);
+
 		console.log("Adding!")
 		console.log(row);
 	}
 
-	const dirpath = "/"+torrent.path+"/"+torrent.name;
 
-	row.querySelector("#name").innerHTML = torrent.name;
-	row.querySelector("#progress").innerHTML = Math.floor(torrent.progress*100);
-	row.querySelector("#downloaded").innerHTML = Math.round(torrent.downloaded/(1024**2));
-	row.querySelector("#uploaded").innerHTML = Math.round(torrent.uploaded/(1024**2));
-	row.querySelector("#link").setAttribute("href", dirpath);
+	// Dynamic properties of the torrent
+	row.querySelector("#progress").innerHTML = Math.floor(torrent.progress*1000)/10;
+	row.querySelector("#downloaded").innerHTML = Math.round(torrent.downloaded/MEBIBYTE);
+	row.querySelector("#uploaded").innerHTML = Math.round(torrent.uploaded/MEBIBYTE);
 
 }
 
+// Update list of torrents on screen every TORRENT_PROGRESS_POLL_PERIOD milliseconds
 function update_all_torrents()
 {
 	get_request("/api/torrent/progress", function(err, result){
@@ -81,10 +97,9 @@ function update_all_torrents()
 			}
 		}
 
-		setTimeout(update_all_torrents, 100);
+		setTimeout(update_all_torrents, TORRENT_PROGRESS_POLL_PERIOD);
 
 	});
-
 }
 
 update_all_torrents();
